6bea09eac3258622819a618443fe2b08eea54051
Paul Chung <PaulChung@cakes-mbp.corp.getcake.com>
2017-01-27 14:42:15 -0800

    added all project files with new encoding logic

---
+++ README.md
@@ -0,0 +1,85 @@
+# CAKE Click Export Tool
+
+**THE CLICK EXPORT TOOL MUST BE USED ON Python 2.7**
+
+In order to run the CAKE Click Export Tool, you will need to follow the directions below:
+
+## Requirements
+* `Python 2.7`
+* `MongoDB`
+* `AWS SQS`
+* `AWS S3`
+
+## Guidelines & Caveats:
+  * Please use Python 2.7 to run the Click Export Tool!
+  * This tool is not meant for LARGE date ranges or LARGE report sizes. Whenever possible, please keep your report queries within one or two days maximum. Remember, you have the ability to queue multiple jobs!
+  * Please consider that it may take hours to complete one queued job.
+  * Use the format 'dd-mm-yyyy' for both the start and end date (the dashes must be included)
+
+## Instructions
+
+1. First execute the following command in your terminal `pip install -r requirements.txt`. This will install all the necessary Python packages to run the tool.
+
+2. Configure your AWS credentials
+  * In your terminal run `aws config`. It will ask you a few questions requiring you to provide an AWS access key and secret key as well as your default region.
+
+3. Fill in each empty quote in `settings.py`
+
+4. Run the web interface
+  * In the main cake click tools directory, run the command 'python run.py', this starts the Flask web server for the web interface.
+  * Navigate to "http://localhost:5000"
+  * Log into the interface using your admin domain address (without 'http://'), and an email address and password for a user that has admin access in your CAKE instance.
+
+Once your credentials are validated, you will land on the welcome page, which will allow you to queue report exports.
+
+5. Scheduling a Click Report Export:
+  * Fill in the start and end date, being mindful of the date format 'dd-mm-yyyy' (the dashes must be included)
+  * Click "Schedule Job"
+  * When the job is successfully added to the queue, the message 'job has been scheduled' will appear underneath the grey well.
+  * If you want to schedule multiple jobs, just add another start and end date combo.
+
+Great! You've scheduled an export. Now in a separate terminal window, navigate to the project and run the command 'python task_runner.py'. The script repeats every minute to check if there are new jobs in the queue and subsequently processes those. Keep 'task_runner.py' open for the duration of the export job.
+
+The Task Runner connects to the SQS queue and processes each job in the queue. Keep in mind that it may take a few hours to complete your export. Once the job is complete it drops the completed CSV in your S3 bucket as well as providing a link to download the report in the web interface.
+
+The csv download link, "Download Report" on the Click Export Tool interface is only valid for 24 hours from the time the queue job completes. This is a limitation of S3. Your csv file will always be accessible from S3.
+
+## Additional Information
+
+### Reading Your Queue Report (Web Interface)
+#### Date Formatting
+The `date format` for the web interface is defaulted to `dd-mm-yyyy`.
+
+#### Job ID
+A `job_id` looks like this `report_10012016_04022016`. It consists of 3 parts: title, start, and end date. The title `report` begins the job_id. The `start date` is in `DDMMYYYY` format; same with the `end date`. For the example `job id`, the csv would be contain the results of January 10th 2016 to February 4th 2016.
+
+#### Job Statuses
+Your queued export job can have one of the following statuses:
+* `Queued`: Your job was successfully added to the SQS queue. When the task runner is executed, it will read from the SQS queue and process each queue item in the order that it was entered.
+* `In Progress`: Your queued job is now being actively exported. In this state, it is best to leave your task runner alive until you receive an updated status.
+* `Success`: Your queue job has completed. The `Download Link` is now visible. Clicking on the link will automatically start a download of your file.
+* `Failed`: Your queue job encountered an error while being exported. Try reading the task runner output, or queue the job once again.
+
+## FAQ
+
+**Q: My report has started processing, but it is now frozen on the same interval for a long period of time. What can I do?**
+
+A: Follow these steps:
+* Cancel the `task_runner.py`.
+* Go to the Click Export Tool interface and requeue the job.
+* Run the `task_runner.py` again.
+
+**Q: I've successfully added an export job in the queue, but when I run `task_runner.py` I get either get an error or a "No Message in Queue" message.**
+
+A: There is usually a short delay period between the time a message is sent to your SQS queue and when you can receive a message to process. Please wait one minute and run `task_runner.py` again to resolve this issue.
+
+**Q: I received the following error message when running `task_runner.py`:**
+
+        Traceback (most recent call last):
+          File "task_runner.py", line 426, in <module>
+            execute_call(response)
+          File "task_runner.py", line 125, in execute_call
+            body = (response["Messages"][0]["Body"]).replace("'", "\"")
+        KeyError: 'Messages'
+
+A: Please wait a few minutes, or manually go into the AWS SQS console and poll your queue. After, run `task_runner.py` again.
---
+++ task_runner.py
@@ -0,0 +1,297 @@
+# -*- coding: utf-8 -*-
+
+import boto3
+import json
+import time
+import copy
+from collections import OrderedDict
+import re
+import csv
+from datetime import datetime, date, timedelta
+import requests
+from flask import Flask, render_template, url_for, request, redirect, g, jsonify, flash, session, Markup
+from run import app
+from pymongo import *
+from bs4 import BeautifulSoup
+from settings import *
+
+
+client = MongoClient(MONGODB_DATABASE['uri'])
+db = client[MONGODB_DATABASE['database_name']]
+
+in_progress = False
+
+def get_country_codes():
+    payload = dict(
+        api_key=API_KEY,
+        )
+
+    endpoint_string = 'http://' + ADMIN_DOMAIN_URL + '/api/1/get.asmx/Currencies'
+    soup = requests.post(endpoint_string,json=payload)
+    r = soup.json()
+
+    country_codes = {}
+
+    for x in r["d"]["currencies"]:
+        abbreviation = x["currency_abbr"]
+        currency_id = str(x["currency_id"])
+        country_codes[currency_id] = abbreviation
+
+    return country_codes
+
+country_codes = get_country_codes()
+
+def return_currency_name(country_id, country_codes):
+    country_id = str(country_id)
+    country_name = ''.join({value for key, value in country_codes.items() if country_id == key})
+    return country_name
+
+
+def receive_message():
+
+    in_progress = True
+    client = boto3.client('sqs')
+    queue_size_response = client.get_queue_attributes(QueueUrl= SQS_QUEUE['url'],
+                                                AttributeNames=['ApproximateNumberOfMessages'])
+    queue_size = queue_size_response["Attributes"]["ApproximateNumberOfMessages"]
+    if queue_size != "0":
+        response = OrderedDict(client.receive_message(QueueUrl = SQS_QUEUE['url'],
+                                            AttributeNames=['Body'],
+                                            MaxNumberOfMessages=1))
+        return response
+    elif queue_size == "0":
+        response = "No Messages in Queue"
+        return response
+
+def delete_message(receipt_handle):
+    client = boto3.client('sqs')
+    response = client.delete_message(QueueUrl= SQS_QUEUE['url'],
+                                     ReceiptHandle=receipt_handle)
+    return response
+
+def enumerate_dates(start, end):
+    start_date = datetime(start.year, start.month, start.day)
+    end_date = datetime(end.year, end.month, end.day)
+    delta = end_date - start_date
+
+    for i in range(delta.days + 1):
+        next_start_date = start_date + timedelta(days=1)
+        for i in range(288):
+            next_start_hour = start_date + timedelta(minutes=5)
+            yield start_date, next_start_hour
+            start_date = next_start_hour
+        start_date=next_start_date
+
+def date_convert_for_csv(date):
+    extract_integers = re.findall('\d+', date)
+    date_string = ''.join(extract_integers)
+    if len(date_string) > 10:
+        date_string = date_string[:10] + '.' + date_string[10:]
+        date_result = (datetime.utcfromtimestamp(float(date_string)) + timedelta(hours=1)).strftime("%d-%m-%YT%H:%M:%S.%f")
+        return date_result
+    else:
+        timestamp_parsed = (datetime.utcfromtimestamp(int(date_string))+ timedelta(hours=1)) + '.000000'
+        date_result = timestamp_parsed.strftime("%d-%m-%YT%H:%M:%S.%f")
+        return date_result
+
+def conversion_time_delta(conversion_date, click_date):
+    conversion_date = datetime.strptime(conversion_date, "%d-%m-%YT%H:%M:%S.%f")
+    click_date = datetime.strptime(click_date, "%d-%m-%YT%H:%M:%S.%f")
+
+    if click_date > conversion_date:
+        time_delta = "1"
+        return time_delta
+
+    elif conversion_date > click_date:
+        time_delta = conversion_date - click_date
+        if time_delta.seconds < 1:
+            time_delta = "1"
+            return time_delta
+        else:
+            return time_delta
+
+def s3_job(filename):
+# expire 86400 seconds is 24 hours
+    s3 = boto3.resource('s3')
+    s3.meta.client.upload_file('temp.csv', S3_BUCKET['name'], '%s.csv' % filename)
+
+    client = boto3.client('s3')
+    url = client.generate_presigned_url('get_object',
+                                        Params={'Bucket': S3_BUCKET['name'],'Key': '%s.csv' % filename},
+                                        ExpiresIn=86400)
+    return url
+
+
+def execute_call(response):
+
+    body = (response["Messages"][0]["Body"]).replace("'", "\"")
+    load_body = json.loads(body)
+
+    start_date = load_body['start_date']
+    end_date = load_body['end_date']
+    job_id = load_body['job_id']
+    created_date = load_body['created_date']
+    receipt_handle = response["Messages"][0]["ReceiptHandle"]
+    delete_message(receipt_handle)
+
+    start_datetime = datetime.strptime(start_date, "%m/%d/%y")
+    end_datetime = datetime.strptime(end_date, "%m/%d/%y")
+
+    collection_name = db[MONGODB_DATABASE['collection_name']]
+    collection_name.update_one({"created_date": created_date}, {"$set": {"status": "In Progress"}})
+
+    try:
+        with open('temp.csv', 'wb') as text_file:
+            writer = csv.writer(text_file)
+            record = 'Click ID', 'Visitor ID', 'Tracking ID', 'Request ID', 'UDID', 'Click Date', \
+                    'Affiliate ID', 'Affiliate Name', 'Advertiser ID', 'Advertiser Name', 'Offer ID', \
+                    'Offer Name', 'Campaign ID', 'Creative ID', 'Creative Name', 'Sub ID 1', 'Sub ID 2', \
+                    'Sub ID 3', 'Sub ID 4', 'Sub ID 5', 'IP Address', 'User Agent', 'Referrer', \
+                    'Request URL', 'Redirect URl', 'Country Code', 'Region Code', 'Language', 'ISP', \
+                    'Device', 'Operating System', 'OS Major', 'OS Minor', 'Browser', 'Browser Major', \
+                    'Browser Minor', 'Disposition', 'Paid Action', 'Currency', 'Amount Paid', \
+                    'Duplicate', 'Duplicate Clicks', 'Total Clicks',
+            writer.writerow(record)
+
+
+
+            date_generator = enumerate_dates(start_datetime, end_datetime)
+
+            for start, end in date_generator:
+                start = datetime.strftime(start, '%m/%d/%Y %H:%M')
+                end = datetime.strftime(end, '%m/%d/%Y %H:%M')
+                print(start, end)
+                payload = dict(
+                    api_key=API_KEY,
+                    start_date=start,
+                    end_date=end,
+                    affiliate_id=0,
+                    advertiser_id=0,
+                    offer_id=0,
+                    campaign_id=0,
+                    creative_id=0,
+                    price_format_id=0,
+                    include_duplicates='False',
+                    include_tests='False',
+                    start_at_row=0,
+                    row_limit=0)
+
+                endpoint_string = 'http://' + ADMIN_DOMAIN_URL + '/api/11/reports.asmx/Clicks'
+                soup = requests.post(endpoint_string,json=payload)
+                print('processing API response')
+                #soup_text = soup.text
+                response = json.loads(soup.text)
+
+                for c in response['d']['clicks']:
+                    click_id = c['click_id']
+                    visitor_id = c['visitor_id']
+                    tracking_id = c['tracking_id']
+                    request_id = c['request_session_id']
+                    udid = ''
+                    if not c['udid'] is None:
+                        udid = c['udid'].encode('utf-8', 'ignore')
+                    click_date = date_convert_for_csv(c['click_date'])
+                    affiliate_id = c['source_affiliate']['source_affiliate_id']
+                    affiliate_name = c['source_affiliate']['source_affiliate_name'].encode('utf-8', 'ignore')
+                    advertiser_id = c['brand_advertiser']['brand_advertiser_id']
+                    advertiser_name = c['brand_advertiser']['brand_advertiser_name'].encode('utf-8', 'ignore')
+                    offer_id = c['site_offer']['site_offer_id']
+                    offer_name = c['site_offer']['site_offer_name'].encode('utf-8', 'ignore')
+                    campaign_id = c['campaign']['campaign_id']
+                    creative_id = c['creative']['creative_id']
+                    creative_name = c['creative']['creative_name'].encode('utf-8', 'ignore')
+                    sub_id_1 = c['sub_id_1'].encode('utf-8', 'ignore')
+                    sub_id_2 = c['sub_id_2'].encode('utf-8', 'ignore')
+                    sub_id_3 = c['sub_id_3'].encode('utf-8', 'ignore')
+                    sub_id_4 = c['sub_id_4'].encode('utf-8', 'ignore')
+                    sub_id_5 = c['sub_id_5'].encode('utf-8', 'ignore')
+                    ip_address = c['ip_address']
+                    user_agent = c['user_agent'].encode('utf-8', 'ignore')
+                    referrer_url = c['referrer_url'].encode('utf-8', 'ignore')
+                    request_url = c['request_url'].encode('utf-8', 'ignore')
+                    redirect_url = c['redirect_url'].encode('utf-8', 'ignore')
+                    country_code = ''
+                    if not c['country'] is None:
+                        country_code = c['country']['country_code'].encode('utf-8', 'ignore')
+                    region = ''
+                    if not c['region'] is None:
+                        region = c['region']['region_code'].encode('utf-8', 'ignore')
+                    language = ''
+                    if not c['language'] is None:
+                        language = c['language']['language_name'].encode('utf-8', 'ignore')
+                    isp = ''
+                    if not c['isp'] is None:
+                        isp = c['isp']['isp_name'].encode('utf-8', 'ignore')
+                    device = ''
+                    if not c['device'] is None:
+                        device = c['device']['device_name'].encode('utf-8', 'ignore')
+                    operating_system = ''
+                    os_major = ''
+                    os_minor = ''
+                    if not c['operating_system'] is None:
+                        operating_system = c['operating_system']['operating_system_name'].encode('utf-8', 'ignore')
+                        os_major = c['operating_system']['operating_system_version']['version_name']
+                        os_minor = c['operating_system']['operating_system_version_minor']['version_name']
+                    browser = ''
+                    browser_major = ''
+                    browser_minor = ''
+                    if not c['browser'] is None:
+                        browser = c['browser']['browser_name'].encode('utf-8', 'ignore')
+                        browser_major = c['browser']['browser_version']['version_name']
+                        browser_minor = c['browser']['browser_version_minor']['version_name']
+                    disposition = c['disposition']
+                    paid_action = ''
+                    if not c['paid_action'] is None:
+                        paid_action = c['paid_action']
+                    currency = ''
+                    amount_paid = ''
+                    if not c['paid'] is None:
+                        currency = currencies[c['paid']['currency_id']]
+                        amount_paid = c['paid']['amount']
+                    duplicate = c['duplicate']
+                    duplicate_clicks = c['duplicate_clicks']
+                    total_clicks = c['total_clicks']
+
+                    record = click_id, visitor_id, tracking_id, request_id, udid, \
+                            click_date, affiliate_id, affiliate_name, advertiser_id, \
+                            advertiser_name, offer_id, offer_name, campaign_id, \
+                            creative_id, creative_name, sub_id_1, sub_id_2, sub_id_3, \
+                            sub_id_4, sub_id_5, ip_address, user_agent, referrer_url, \
+                            request_url, redirect_url, country_code, region, language, \
+                            isp, device, operating_system, os_major, os_minor, browser, \
+                            browser_major, browser_minor, disposition, paid_action, \
+                            currency, amount_paid, duplicate, duplicate_clicks, total_clicks,
+
+                    writer.writerow(record)
+                print('Processing complete')
+
+
+        file_link = s3_job(job_id)
+        print('REPORT SUCCESSFULLY CREATED')
+        print(file_link)
+        print('CHECKING FOR ADDITIONAL QUEUED EXPORTS')
+
+        collection_name = db[MONGODB_DATABASE['collection_name']]
+        collection_name.update_one({"created_date": created_date}, {"$set": {"status": "Success", "file_link": file_link }})
+        in_progress = False
+
+    except Exception:
+        collection_name = db[MONGODB_DATABASE['collection_name']]
+        collection_name.update_one({"created_date": created_date}, {"$set": {"status": "Failed"}})
+        in_progress = False
+        raise
+
+start_time = time.time()
+
+if __name__ == "__main__":
+    while True:
+        if in_progress == True:
+            time.sleep(60.0 - ((time.time() - start_time) % 60.0))
+        else:
+            response = receive_message()
+            if response == "No Messages in Queue":
+                print(response)
+                time.sleep(60.0 - ((time.time() - start_time) % 60.0))
+            else:
+                execute_call(response)
+                time.sleep(60.0 - ((time.time() - start_time) % 60.0))
